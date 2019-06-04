e2d959848cd83d90024282324d9c97a3603d2401
Administrator <977504087@qq.com>
2019-06-04 09:10:06 +0800

    two two

---
+++ src/main/java/yhg.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class yhg {
+    public static void main(String[] args) {
+        Scanner yhg=new Scanner(System.in);
+        while (yhg.hasNext()) {
+            //利用nextXXX()方法输出内容
+            String str = yhg.next();
+            System.out.println(str);
+        }
+    }
+}
