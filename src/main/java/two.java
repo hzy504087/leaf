660e11a1a76f5805e5a299e45f4d96b9a19d7db1
leaf <977504087@qq.com>
2019-06-06 11:13:33 +0800

    two two ad

---
+++ src/main/java/two.java
@@ -0,0 +1,19 @@
+public class two {
+    public static void main(String[] args) {
+        boolean inn=true;
+        boolean itt=true;
+        if(inn&itt==true){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+        int in=50;
+        String it=in>60?"及格":"不及格";
+        System.out.println(it);
+        int a=40;
+        int b=50;
+
+    }
+
+
+}
--- src/main/java/yhg.java
+++ src/main/java/yhg.java
@@ -2,13 +2,25 @@ import java.util.Scanner;
 
 public class yhg {
     public static void main(String[] args) {
-        Scanner yhg=new Scanner(System.in);
-        while (yhg.hasNext()) {
-            //利用nextXXX()方法输出内容
-            String str = yhg.next();
-            System.out.println(str);
-            System.out.println("+++++++++++++++++++++");
-            System.out.println("+++++++++++++++++++++");
+//        Scanner yhg=new Scanner(System.in);
+//        String name=yhg.nextLine();
+//        int sex=yhg.nextInt();
+//        System.out.println(name);
+        Integer a=10;
+        int b=a+10;
+        double bb=90;
+        int c=a;
+        System.out.println(b);
+//        角度转化为弧度
+        double i=Math.toRadians(bb);
+//        弧度转角度
+        double aa=Math.toDegrees(i);
+        System.out.println(i);
+        System.out.println(aa);
+        System.out.println(Math.atan2(i,aa));
+        if(Character.isLetter('c')){
+            System.out.println(a);
+
         }
     }
 }
