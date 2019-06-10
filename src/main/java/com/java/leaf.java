9a0bee7484e54a804dd2c396ad877696d576d72f
leaf <977504087@qq.com>
2019-06-10 09:24:01 +0800

    import

--- src/main/java/com/java/leaf.java
+++ src/main/java/com/java/leaf.java
@@ -3,9 +3,12 @@ package com.java;
 import com.javac.yhg;
 //enter alt
 //import com.javac.yhg;
+//获取定义的变量类
 public class leaf {
     public static void main(String[] args) {
         yhg yhg=new yhg();
+        yhg.name="leaf";
+        yhg.aaa=true;
         System.out.println(yhg.aaa);
     }
 }
