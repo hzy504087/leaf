41e83bc6945b8c94188efb2a4a94b4be3f57eacc
leaf <977504087@qq.com>
2019-06-10 09:13:06 +0800

    package

---
+++ src/main/java/com/java/leaf.java
@@ -0,0 +1,11 @@
+package com.java;
+
+import com.javac.yhg;
+//enter alt
+//import com.javac.yhg;
+public class leaf {
+    public static void main(String[] args) {
+        yhg yhg=new yhg();
+        System.out.println(yhg.aaa);
+    }
+}
