578afe2b41f1c4a7cda682116366ed184415b503
leaf <977504087@qq.com>
2019-06-12 17:41:27 +0800

    polymer

---
+++ src/main/java/com/javac/polymmer.java
@@ -0,0 +1,32 @@
+package com.javac;
+
+public class polymmer {
+    public static void main(String[] args) {
+        donwu ff=new donwu();
+        ff.a();
+        cat fff=new cat();
+        gg(fff);
+
+    }
+    static void gg(donwu ff){
+        ff.a();
+    }
+
+}
+class donwu{
+    public void a(){
+        System.out.println("111");
+    }
+
+}
+class cat extends donwu{
+    public void a(){
+        System.out.println("222");
+    }
+
+}
+class dog extends donwu{
+    public  void a(){
+        System.out.println("333");
+    }
+}
